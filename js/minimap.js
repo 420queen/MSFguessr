753938f5628c6321ccd8c5ddd615948c955bd0e9
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-16 23:49:25 +0200

    Update minimap.js

--- js/minimap.js
+++ js/minimap.js
@@ -4,12 +4,22 @@ function mminitialize() {
     const protomapsLayer = protomapsL.leafletLayer({
         url: 'https://api.protomaps.com/tiles/v3/{z}/{x}/{y}.mvt?key=d8045d15a0243832',
         flavor: 'light',
-        lang: 'fr'
     });
+
+    var myCustomIcon = L.icon({
+    iconUrl: 'img/marker-icon.png',
+    iconRetinaUrl: 'img/marker-icon-2x.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41], // taille par défaut
+    iconAnchor: [12, 41], // centre-bas de l’icône (point de clic)
+    popupAnchor: [0, -41], // optionnel : popup au-dessus de la pointe
+    shadowSize: [41, 41] // taille par défaut de l’ombre
+});
+
     protomapsLayer.addTo(mymap);
 
     // Place off-screen or hidden
-    guess2 = L.marker([0, 0], { opacity: 0 }).addTo(mymap);
+    guess2 = L.marker([0, 0], { icon: myCustomIcon, opacity: 0 }).addTo(mymap);
     window.guessLatLng = null;
 
     mymap.on("click", function(e) {
