393ce5bd4512a18f895ad8b5cd47f2e36f463d37
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-16 23:34:51 +0200

    Update roundmap.js

--- js/roundmap.js
+++ js/roundmap.js
@@ -1,34 +1,42 @@
 function rminitialize() {
     roundmap = L.map("roundMap").setView([30, 10], 1);
 
-   const protomapsLayer = protomapsL.leafletLayer({
+    const protomapsLayer = protomapsL.leafletLayer({
         url: 'https://api.protomaps.com/tiles/v3/{z}/{x}/{y}.mvt?key=d8045d15a0243832',
-       flavor: 'light', // 
-          lang: 'fr' // 
+        flavor: 'light',
+        lang: 'fr'
     });
     protomapsLayer.addTo(roundmap);
 
-    var guessIcon = L.icon({
+    const guessIcon = L.icon({
         iconUrl: "img/guess.png",
         iconAnchor: [45, 90]
     });
 
-    var actualIcon = L.icon({
+    const actualIcon = L.icon({
         iconUrl: "img/actual.png",
         iconAnchor: [45, 90]
     });
 
-    guess = L.marker([-999, -999], {
-        icon: guessIcon
-    }).addTo(roundmap)
+    guess = L.marker([0, 0], { icon: guessIcon, opacity: 0 }).addTo(roundmap);
+    actual = L.marker([0, 0], { icon: actualIcon, opacity: 0 }).addTo(roundmap);
 
-    actual = L.marker([-999, -999], {
-        icon: actualIcon
-    }).addTo(roundmap)
+    if (
+        window.guessLatLng &&
+        typeof window.guessLatLng.lat === "number" &&
+        typeof window.guessLatLng.lng === "number" &&
+        window.actualLatLng &&
+        typeof window.actualLatLng.lat === "number" &&
+        typeof window.actualLatLng.lng === "number"
+    ) {
+        guess.setLatLng(window.guessLatLng).setOpacity(1);
+        actual.setLatLng(window.actualLatLng).setOpacity(1);
 
-    guess.setLatLng(window.guessLatLng);
-    actual.setLatLng(window.actualLatLng);
-    roundmap.fitBounds(L.latLngBounds(guess.getLatLng(), actual.getLatLng()), {
-        padding: [50, 50]
-    });
-};
+        roundmap.fitBounds(
+            L.latLngBounds([window.guessLatLng, window.actualLatLng]),
+            { padding: [50, 50] }
+        );
+    } else {
+        console.warn("Invalid guess or actual location:", window.guessLatLng, window.actualLatLng);
+    }
+}
