050482a146ba5409a35d8d97358ad5cafc64d4d9
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-17 09:34:15 +0200

    Update minimap.js

--- js/minimap.js
+++ js/minimap.js
@@ -8,7 +8,17 @@ function mminitialize() {
         maxZoom: 18
     }).addTo(mymap);
 
-    guess2 = L.marker([-999, -999]).addTo(mymap);
+    var myCustomIcon = L.icon({
+    iconUrl: 'img/marker-icon.png',
+    iconRetinaUrl: 'img/marker-icon-2x.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41], // taille par défaut
+    iconAnchor: [12, 41], // centre-bas de l’icône (point de clic)
+    popupAnchor: [0, -41], // optionnel : popup au-dessus de la pointe
+    shadowSize: [41, 41] // taille par défaut de l’ombre
+});
+
+    guess2 = L.marker([0, 0], { icon: myCustomIcon, opacity: 0 }).addTo(mymap);
     guess2.setLatLng({lat: -999, lng: -999});
 
     mymap.on("click", function(e) {
