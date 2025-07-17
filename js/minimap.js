558ff75818e6d49b7505e25fe76c17cef922eaad
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-17 09:42:16 +0200

    Update minimap.js

--- js/minimap.js
+++ js/minimap.js
@@ -9,9 +9,9 @@ function mminitialize() {
     }).addTo(mymap);
 
     var myCustomIcon = L.icon({
-    iconUrl: 'img/marker-icon.png',
-    iconRetinaUrl: 'img/marker-icon-2x.png',
-    shadowUrl: 'img/marker-shadow.png',
+    iconUrl: '.img/marker-icon.png',
+    iconRetinaUrl: '.img/marker-icon-2x.png',
+    shadowUrl: '.img/marker-shadow.png',
     iconSize: [25, 41], // taille par défaut
     iconAnchor: [12, 41], // centre-bas de l’icône (point de clic)
     popupAnchor: [0, -41], // optionnel : popup au-dessus de la pointe
